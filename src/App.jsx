ce29aadce2f318269acf35996aa321cd1fb5c740
kavyavannur14 <kavyavannur14@gmail.com>
2025-10-27 17:05:41 +0530

    Add all project files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from './components/Todo'
+import TodoItems from './components/TodoItems'
+
+const App = () => {
+  return (
+    <div>
+       <Todo/>
+       <TodoItems/>
+    </div>
+  )
+}
+
+export default App
